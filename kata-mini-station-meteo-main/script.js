9b385278af91709295c42e26fe5f056cc4180cb4
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-08-01 10:14:19 +0200

    etape 1 et 2 valider

--- kata-mini-station-meteo-main/script.js
+++ kata-mini-station-meteo-main/script.js
@@ -1,1 +1,34 @@
 // A toi de jouer pour cette partie :-) Happy coding !
+
+
+const cityInput = document.getElementById("cityInput")
+const divCitySearch = document.querySelector(".city-search")
+const loading = document.getElementById("details")
+const divGps = document.getElementById("gps")
+
+console.log(divCitySearch)
+
+
+divCitySearch.addEventListener("click", () => {
+    const inputValue = cityInput.value
+    localisation(inputValue)
+})
+
+
+const localisation = async (inputValue) => {
+    try {
+        const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${inputValue}&format=json&addressdetails=1&limit=1`)
+        const data = await response.json()
+        loading.style.display= "none"
+
+        document.getElementById("city").innerText=inputValue
+        data.forEach(element => {
+            divGps.innerText = `Coordonnées GPS : ${element.lat},${element.lon}`
+        });
+
+        return data
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
