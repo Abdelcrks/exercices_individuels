175039b8d8a61de1e553fe3a5efd1f8476974497
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-08-01 11:34:11 +0200

    etape 4 erreurs gerer les erreurs terminé

--- kata-mini-station-meteo-main/script.js
+++ kata-mini-station-meteo-main/script.js
@@ -2,15 +2,14 @@
 
 
 const cityInput = document.getElementById("cityInput")
-const divCitySearch = document.querySelector(".city-search")
 const loading = document.getElementById("details")
 const divGps = document.getElementById("gps")
+console.log(cityInput)
 
-console.log(divCitySearch)
 
-
-divCitySearch.addEventListener("click", () => {
+document.querySelector(".city-search button").addEventListener("click", () => {
     const inputValue = cityInput.value
+
     fetchLocalisation(inputValue)
 })
 
@@ -20,7 +19,12 @@ const fetchLocalisation = async (inputValue) => {
         const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${inputValue}&format=json&addressdetails=1&limit=1`)
         const data = await response.json()
         loading.style.display= "none"
-
+        
+        if (data.length === 0){
+            document.getElementById("city").innerText = "Ville introuvable"
+            divGps.innerText = ""
+            document.getElementById("temperature").innerText = ""
+        }
         ShowApiLocalisation(data)
         return data
     }
@@ -29,25 +33,29 @@ const fetchLocalisation = async (inputValue) => {
     }
 }
 
-
 const ShowApiLocalisation = (data) => {
 
     data.forEach(element => {
-        document.getElementById("city").innerText= element.address.city
+        document.getElementById("city").innerText= `${element.name}, ${element.address.country}`
         const latitude = element.lat
         const longitude = element.lon
         divGps.innerText = `Coordonnées GPS : ${latitude},${longitude}`
         fetchWeather(latitude,longitude)
     })
+    return
 }
 
 const fetchWeather = async (latitude,longitude) => {
     try {
+        loading.style.display="block"
         const response= await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,precipitation,relative_humidity_2m`)
         const data = await response.json()
+        loading.style.display="none"
         document.getElementById("temperature").innerText= `${data.current.temperature_2m}°C`
         return data
     } catch (error) {
         console.log(error)
     }
 }
+
+
