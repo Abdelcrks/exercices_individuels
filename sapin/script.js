1a033c4aa146391c95b3a41f1761e5d1a0df05bc
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-09-12 11:30:13 +0200

    fct afficheretoiles

---
+++ sapin/script.js
@@ -0,0 +1,21 @@
+function afficherEtoiles(n){
+
+    let etoiles = ""
+    for (let i = 0; i <n; i++){
+        etoiles+= "*"
+    }
+
+    console.log(etoiles)
+
+}
+
+afficherEtoiles(2)
+afficherEtoiles(5)
+
+  
+
+function afficherRectangle(hauteur,largeur){
+    
+}
+
+afficherRectangle(5,5)
