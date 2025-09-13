d6494b91cc811acae2d25a9001312437573ac20c
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-09-13 02:05:57 +0200

    etape  1.3

--- sapin/script.js
+++ sapin/script.js
@@ -21,4 +21,24 @@ function afficherRectangle(hauteur,largeur){
     }
 }
 
-afficherRectangle(5,9)
+// afficherRectangle(5,9)
+
+
+function afficherTriangleDroite(n) {
+    // En s'inspirant de la fonction afficherRectangle(),
+    // comment afficher des lignes qui "s'allongent" un peu plus à chaque itération ?
+
+    for(let i=0 ; i<n; i++){
+       let ligne = ""
+       
+       for(let etoile = 0 ; etoile<i ; etoile++){
+        ligne+= "*"
+       }
+       ligne+= "\\ "
+       console.log(ligne);
+    }
+    
+  }
+  
+  afficherTriangleDroite(5)
+
